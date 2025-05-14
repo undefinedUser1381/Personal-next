701199465be0f6cb453b714b01dddcddf5eda26b
erfan <erfanghezi5050@gmail.com>
2025-05-14 23:19:54 +0330

    Update mobile sidebar

--- components/MobileSidebar/MobileSidebar.tsx
+++ components/MobileSidebar/MobileSidebar.tsx
@@ -7,7 +7,7 @@ interface MobileSidebarProps {
 
 function MobileSidebar({isOpenMobileMenu , onClose} : MobileSidebarProps) {
   return (
-    <div className={`block md:hidden transition-all duration-200 fixed p-5 ${isOpenMobileMenu ? "left-0" : "left-[-100%]"} top-0 bottom-0 bg-white dark:bg-[#02233d] w-full`}>
+    <div className={`block md:hidden transition-all duration-200 z-50 fixed p-5 ${isOpenMobileMenu ? "left-0" : "left-[-100%]"} top-0 bottom-0 bg-white dark:bg-[#02233d] w-full`}>
      <div onClick={onClose} className="w-full text-darkText">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer w-[2rem] h-[2rem]">
         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />

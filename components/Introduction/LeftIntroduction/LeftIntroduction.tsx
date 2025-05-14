ea39e050c1454af226564736665aaadf72ca9f65
erfan <erfanghezi5050@gmail.com>
2025-05-14 23:22:46 +0330

    Add motion

--- components/Introduction/LeftIntroduction/LeftIntroduction.tsx
+++ components/Introduction/LeftIntroduction/LeftIntroduction.tsx
@@ -1,12 +1,20 @@
+"use client";
+import { motion } from "framer-motion";
 
 export default function LeftIntroduction() {
   return (
-    <div className="rounded-full w-[35%] h-[25rem] overflow-hidden border-dashed border-3 border-primaryLight">
+    <motion.div
+      initial={{ x: -200, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: -200, opacity: 0 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+      className="rounded-full w-[70%] sm:w-[40%] md:w-[40%] lg:w-[45%] xl:w-[35%] h-[17.5rem] md:h-[24rem] xl:h-[26rem] overflow-hidden border-dashed border-2 border-primaryLight"
+    >
       <img
         className="object-cover w-full h-full"
         src="/images/me.jpg"
         alt="Picture of developer"
       />
-    </div>
+    </motion.div>
   );
 }

08d975ba3f542696909ec8f6a2bd26a49e779155
erfan <erfanghezi5050@gmail.com>
2025-05-14 23:23:11 +0330

    Updated ...

--- components/Introduction/Introduction.tsx
+++ components/Introduction/Introduction.tsx
@@ -4,7 +4,7 @@ import RightIntroduction from "./RightIntroduction/RightIntroduction";
 
 export default function Introduction() {
   return (
-    <section className="flex items-center justify-evenly mt-[5rem] h-auto w-full">
+    <section className="flex flex-col lg:flex-row gap-14 md:gap-6 md:flex items-center justify-evenly mt-10 lg:mt-[5rem] h-auto w-full">
       <LeftIntroduction />
       <RightIntroduction />
     </section>

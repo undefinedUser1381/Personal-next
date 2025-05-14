02f50cad54f13a865f81d1f8f2e6db1b4c4a6069
erfan <erfanghezi5050@gmail.com>
2025-05-14 23:21:58 +0330

    Add skills component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import BackgroundBubble from "../../components/Introduction/BackgroundBubble/BackgroundBubble";
 import Introduction from "../../components/Introduction/Introduction";
 import Navbar from "../../components/Navabr/Navbar";
+import Skills from "../../components/Skills/Skills";
 import ThemeSwitcherContainer from "../../components/ThemeSwitcherContainer/ThemeSwitcherContainer";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <main className="lg:container relative z-10 lg:mx-auto lg:px-16">
         <Introduction />
         <BackgroundBubble />
+        <Skills />
       </main>
       <ThemeSwitcherContainer />
     </>

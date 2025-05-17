4bfa6f28827bfa5bb5e9e2f70f052b4ec2d00c1a
erfan <erfanghezi5050@gmail.com>
2025-05-17 16:07:54 +0330

    Add skills component

---
+++ components/Skills/Skills.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import SkillsBox from './SkillsBox/SkillsBox'
+
+export default function Skills() {
+ 
+  const skills = [
+    { id: 1 , skillRate : 4 , title : "Typescript" , src : "/images/skills/typescript-logo-svgrepo-com.svg" },
+    { id: 2 , skillRate : 4 , title : "Javascript" , src : "/images/skills/javascript-svgrepo-com.svg" },
+    { id: 3 , skillRate : 3 , title : "Next js" , src : "/images/skills/next-16-svgrepo-com.svg" },
+    { id: 4 , skillRate : 4 , title : "React js" , src : "/images/skills/react-svgrepo-com.svg" },
+    { id: 5 , skillRate : 5 , title : "Axios" , src : "/images/skills/axios.svg" },
+    { id: 6 , skillRate : 5 , title : "React form" , src : "/images/skills/react-hook-form-logo-only.svg" },
+    { id: 7 , skillRate : 4 , title : "Tailwind" , src : "images/skills/tailwind-svgrepo-com.svg" },
+    { id: 8 , skillRate : 4 , title : "Redux" , src : "images/skills/redux-svgrepo-com.svg" },
+  ]
+
+  return (
+    <div id='skills' className='mt-[4rem] lg:mt-[5rem] px-2.5'>
+       <div className='flex items-center justify-start gap-2'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 dark:text-primaryDark text-primaryLight">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+         </svg>
+        <h4 className='title-section'>Skills</h4>
+       </div>
+       <div className='flex items-center mt-12 justify-start flex-wrap gap-10 xl:gap-9'>
+         {
+            skills.map((skill) => (
+                <SkillsBox key={skill.id} {...skill}/>
+            ))
+         }
+       </div>
+    </div>
+  )
+}
+
---
+++ components/Skills/SkillsBox/SkillsBox.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+
+interface SkillProps {
+  id: number;
+  src: string;
+  title: string;
+  skillRate: number;
+}
+
+export default function SkillsBox({ id, skillRate, src, title}: SkillProps) {
+
+  return (
+    <div className="w-full gap-5.5 rounded-xl flex dark:bg-customCartDark bg-customCartlight items-center justify-start md:w-[47%] lg:w-[30%] xl:w-[16rem] md:h-[7rem] md:justify-center px-3.5 h-[6rem]">
+      <div>
+        <Image width={50} height={50} src={src} alt={title} />
+      </div>
+      <div className="w-3/3 flex flex-row md:flex-col md:w-auto justify-between gap-2.5">
+        <h5 className="dark:text-white text-black">{title}</h5>
+        <div className="flex gap-1.5 items-center">
+          {new Array(skillRate).fill(0).map((item,i) => (
+            <FaStar key={i + 1} className="fill-amber-400" />
+          ))}
+            {new Array(5 - skillRate).fill(0).map((item,i) => (
+            <FaRegStar key={i + 1} className="fill-amber-400" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}

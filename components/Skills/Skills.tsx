c2eb78ee92ef8981f0962776708eae12f560bbb6
erfan <erfanghezi5050@gmail.com>
2025-05-18 11:41:00 +0330

    Rewrite some styles

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -15,8 +15,8 @@ export default function Skills() {
   ]
 
   return (
-    <div id='skills' className='mt-[4rem] lg:mt-[5rem] px-2.5'>
-       <div className='flex items-center justify-start gap-2'>
+    <div id='skills' className='section-container'>
+       <div className='section-title__box'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 dark:text-primaryDark text-primaryLight">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
          </svg>

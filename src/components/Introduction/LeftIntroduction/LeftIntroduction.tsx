a44d192d2fd37f04f1d278bcfc8c7ebbc286bc38
erfan <erfanghezi5050@gmail.com>
2025-05-21 20:23:00 +0330

    Update width of image

--- src/components/Introduction/LeftIntroduction/LeftIntroduction.tsx
+++ src/components/Introduction/LeftIntroduction/LeftIntroduction.tsx
@@ -8,7 +8,7 @@ export default function LeftIntroduction() {
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: -200, opacity: 0 }}
       transition={{ duration: 0.5, ease: "easeOut" }}
-      className="rounded-full w-[70%] sm:w-[40%] md:w-[40%] lg:w-[45%] xl:w-[35%] h-[17.5rem] md:h-[24rem] xl:h-[26rem] overflow-hidden border-dashed border-2 border-primaryLight"
+      className="rounded-full w-[70%] sm:w-[40%] md:w-[40%] lg:w-[45%] xl:w-[30%] h-[17.5rem] md:h-[24rem] xl:h-[26rem] overflow-hidden border-dashed border-2 border-primaryLight"
     >
       <img
         className="object-cover w-full h-full"
